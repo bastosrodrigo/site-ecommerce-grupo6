5bba6cf30c0f794e3f6217b47007158eaf37bf4a
fernandameirelles <fagdemeirelles@gmail.com>
2023-05-04 18:45:59 -0300

    correção do cadastro e login

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* ##### Slide página inicial  ####### */
+/* ##### Slide página inicial  ####### /
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -14,44 +14,43 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 
-/* #### javascript da loja #### */
+/ #### javascript da loja #### */
 // Pega as informações da localStorage
 document.getElementById("nomeCliente").innerHTML = localStorage.getItem("nomeCliente");
 
 function checaUsuarioLogado() { // checa se usuario está logado quando abre, se estiver retorna true
-    return localStorage.getItem("idCliente") !== null;
+  return localStorage.getItem("idCliente") !== null;
 }
 
 // checar usuario logado
 if (checaUsuarioLogado()) {
-    // se sim, remove o "display: none" da div "logado"
-    document.getElementById("logado").setAttribute("style", "display:flex")
-    // e coloque o nome do usuário no elemento "nomeCliente"
-    document.querySelector('#nomeCliente').innerText = localStorage.getItem("nomeCliente");
+  // se sim, remove o "display: none" da div "logado"
+  document.getElementById("logado").setAttribute("style", "display:flex")
+  // e coloque o nome do usuário no elemento "nomeCliente"
+  document.querySelector('#nomeCliente').innerText = localStorage.getItem("nomeCliente");
 } else {
-    // se não, adiciona o estilo "display: none" para o div Logado
-    document.querySelector('.logado').style.display = "none";
-    document.getElementById("logoutBtn").style.display = "none";
-
+  // se não, adiciona o estilo "display: none" para o div Logado
+  document.querySelector('.logado').style.display = "none";
+  document.getElementById("logoutBtn").style.display = "none";
 }
 
 // função para limpar a sessionStora e redireciona para o login
 function logout() {
-    localStorage.clear();
-    window.location.href = "login.html";
+  localStorage.clear();
+  window.location.href = "login.html";
 
-    checaUsuarioLogado(); 
+  checaUsuarioLogado();
 }
