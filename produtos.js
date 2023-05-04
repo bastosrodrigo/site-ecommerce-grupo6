3354dbfa2003e8f15b518f6aec48cede4bff472f
caioOliveiraF <caio.o.12oferreirao@gmail.com>
2023-05-04 18:11:23 -0300

    produtos e card de produtos na home

---
+++ produtos.js
@@ -0,0 +1,286 @@
+/* ######  produtos  ####### */
+const produtos = [
+    {
+        id: 1,
+        nome: "Camisa Flamengo",
+        categoria: "masculino",
+        src: "./src/2.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 250,00",
+    },
+    {
+        id: 2,
+        nome: "Camisa Fluminense",
+        categoria: "masculino",
+        src: "./src/3.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 250,00",   
+    },
+    {
+        id: 3,
+        nome: "Camisa Botafogo",
+        categoria: "masculino",
+        src: "./src/4.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 250,00",
+    },
+    {
+        id: 4,
+        nome: "Camisa Real Madrid",
+        categoria: "masculino",
+        src: "./src/5.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star',
+        preco: "R$ 499,99",
+    },
+    {
+        id: 5,
+        nome: "Camisa Paris Saint-Germain",
+        categoria: "masculino",
+        src: "./src/6.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 499,99",
+    },
+    {
+        id: 6,
+        nome: "Camisa Barcelona",
+        categoria: "masculino",
+        src: "./src/7.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star',
+        preco: "R$ 350,49",
+    },
+    {
+        id: 7,
+        nome: "Camisa Juventus",
+        categoria: "masculino",
+        src: "./src/8.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 379,99",
+    },
+    {
+        id: 8,
+        nome: "Camisa Palmeiras",
+        categoria: "masculino",
+        src: "./src/9.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 284,90",
+    },
+    {
+        id: 9,
+        nome: "Camisa Grêmio",
+        categoria: "masculino",
+        src: "./src/10.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 159,99",
+    },
+    {
+        id: 10,
+        nome: "Kit Santos Infantil",
+        categoria: "infantil",
+        src: "./src/11.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 189,99",
+    },
+    {
+        id: 11,
+        nome: "Kit Fluminense Infantil",
+        categoria: "infantil",
+        src: "./src/12.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 237,49",
+    },
+    {
+        id: 12,
+        nome: "Kit Internacional Infantil",
+        categoria: "infantil",
+        src: "./src/13.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 309,00",
+    },
+    {
+        id: 13,
+        nome: "Kit Recife Infantil",
+        categoria: "infantil",
+        src: "./src/14.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 109,99",
+    },
+    {
+        id: 14,
+        nome: "Kit Flamengo Infantil",
+        categoria: "infantil",
+        src: "./src/15.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 284,99",
+    },
+    {
+        id: 15,
+        nome: "Camisa Flamengo Feminino",
+        categoria: "feminino",
+        src: "./src/16.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 297,49",
+    },
+    {
+        id: 16,
+        nome: "Camisa Vasco Feminino",
+        categoria: "feminino",
+        src: "./src/17.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star',
+        preco: "R$ 189,90",
+    },
+    {
+        id: 17,
+        nome: "Camisa Corinthians Feminino",
+        categoria: "feminino",
+        src: "./src/18.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 179,90",
+    },
+    {
+        id: 18,
+        nome: "Camisa Real Madrid Feminino",
+        categoria: "feminino",
+        src: "./src/19.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 199,99",  
+    },
+    {
+        id: 19,
+        nome: "Camisa Grêmio Feminino",
+        categoria: "feminino",
+        src: "./src/20.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 149,90",
+    },
+    {
+        id: 20,
+        nome: "Camisa Palmeiras Feminino",
+        categoria: "feminino",
+        src: "./src/21.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 94,90",
+    },
+    {
+        id: 21,
+        nome: "Camisa Internacional Feminino",
+        categoria: "feminino",
+        src: "./src/22.png",
+        estrela1:'bx bxs-star',
+        estrela2:'bx bxs-star',
+        estrela3:'bx bxs-star',
+        estrela4:'bx bxs-star',
+        estrela5:'bx bxs-star-half',
+        preco: "R$ 297,49",
+    }
+]
+
+const listaElemento = document.getElementById("lista");
+
+function renderizarItem(produto) {
+    const divElemento = document.createElement("div");
+    divElemento.innerHTML = `
+            <div class="prod">
+                <a href="#"><img src="${produto.src}" alt=""></a>
+                <div class="estrelas">
+                    <i class='${produto.estrela1}' ></i>
+                    <i class='${produto.estrela2}' ></i>
+                    <i class='${produto.estrela3}' ></i>
+                    <i class='${produto.estrela4}' ></i>
+                    <i class='${produto.estrela5}'></i>
+                </div>
+                <div class="preco">
+                    <h4>${produto.nome}</h4>
+                    <p>${produto.preco}</p>
+                </div>
+            </div>`;
+    return divElemento;
+  }
+
+function renderizarLista(lista) {
+    lista.forEach(function(item) {
+      const divElemento = renderizarItem(item);
+      listaElemento.appendChild(divElemento);
+    });
+  }
+
+renderizarLista(produtos);
